ee0101415183a8dcda0cac1b2c635654a5249a24
SanskarX10 <44228185+SanskarX10@users.noreply.github.com>
2022-01-29 22:05:21 +0530

    Create 1773. Count Items Matching a Rule.py

---
+++ leetcode/Easy/1773. Count Items Matching a Rule.py
@@ -0,0 +1,15 @@
+"""
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+"""
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        return sum((ruleKey, ruleValue) in (("type", t), ("color", c), ("name", n)) for t, c, n in items)
+        
