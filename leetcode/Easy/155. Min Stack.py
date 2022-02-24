13fd73f3be2b55d015353fca4cb9b1fa461c19dd
SanskarX10 <44228185+SanskarX10@users.noreply.github.com>
2022-02-24 19:11:45 +0530

    Create 155. Min Stack.py

---
+++ leetcode/Easy/155. Min Stack.py
@@ -0,0 +1,31 @@
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+
+"""
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+        
